60fee823f91d828cdd1a2b36526e13c30c60a93b
Gustavo Feller <gjfeller@inf.ufrgs.br>
2017-06-23 14:42:02 -0300

    Implemented MDS, but it is too slow

--- public/javascripts/dimReductionMethods.js
+++ public/javascripts/dimReductionMethods.js
@@ -7,42 +7,106 @@ dimReducMethods.PCA = function PCA(dataset) {
 
     var modifiedDataset = meanSubtractionProcess(dataset);
 
-    console.log(dataset);
-    console.log(modifiedDataset);
+    //console.log(dataset);
+    //console.log(modifiedDataset);
 
     var covMatrix = calculateCovarianceMatrix(modifiedDataset);
 
 
     
     var vectorBase = numeric.transpose(getLargerEigenvectors(covMatrix, 2));
-    console.log(vectorBase);
+    //console.log(vectorBase);
 
     var dataCoordinates = numeric.dot(modifiedDataset, vectorBase);
-    console.log(dataCoordinates);
+    //console.log(dataCoordinates);
     return dataCoordinates;
     
 }
 
 dimReducMethods.MDS = function MDS(dataset) {
 
+    var distanceMatrix = calculateDistanceMatrix(dataset);
+    var P = numeric.pow(distanceMatrix);
 
+    var identityMatrix = numeric.identity(P.length);
+
+    //var matrixOfOnes = numeric.rep([P.length, P.length], 1);
+    //var dividedMatrix = numeric.div(matrixOfOnes, P.length);
+
+    var J = numeric.sub(identityMatrix, numeric.div(numeric.rep([P.length, P.length], 1), P.length));
+    //console.log(J);
+    var B = numeric.div(numeric.dot(J, numeric.dot(P,J)),-2)
+    //console.log(B);
+
+    /*var t0 = performance.now();
+    var vectorBase = numeric.transpose(getLargerEigenvectors(B, 2));
+    var t1 = performance.now();
+    console.log("Call to getLargerEigenvectors took " + millisToMinutesAndSeconds(t1 - t0));*/
+
+    /*var dataCoordinates = numeric.dot(dataset, vectorBase);
+    console.log(dataCoordinates);
+    return dataCoordinates;*/
+    //console.log(distanceMatrix);
 
 }
 
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
 
+function euclideanDistance(v1, v2) {
+    
+        var sum = 0;
+        for(var i = 0; i < v1.length; i++) {
+            sum = sum + Math.pow(v1[i]-v2[i], 2);
+        }
+        return Math.sqrt(sum);
+}
 
 function calculateDistanceMatrix(matrix) {
+
+    var rollCallCount = matrix.length;
+    var distanceMatrix = new Array(rollCallCount);
+    for(var i = 0; i < rollCallCount; i++) {
+        distanceMatrix[i] = new Array(rollCallCount); 
+    }
     
-    //math.distance(v1, v2);
+    for(var i = 0; i < distanceMatrix.length; i++){
+        for(var j = 0; j < distanceMatrix[i].length; j++) {
+            distanceMatrix[i][j] = 0;
+            //console.log(distanceMatrix[i][j]);
+        }
+    }
+    //console.log(distanceMatrix);
+    for(var i = 0; i < matrix.length; i++) {
+        for(var j = i+1; j < matrix.length; j++) {
+            var v1 = matrix[i];
+            var v2 = matrix[j];
+            distanceMatrix[i][j] = euclideanDistance(v1, v2);
+        }
+    }
+
+    for(var i = 1; i < rollCallCount; i++) {
+        for(var j = 0; j < i; j++) {
+            distanceMatrix[i][j] = distanceMatrix[j][i];
+        }
+    }
+
+    //console.log(distanceMatrix);
+    return distanceMatrix;
 }
 
 function getLargerEigenvectors(matrix, d) {
 
+    console.log('Before eig');
     var eigen = numeric.eig(matrix);
-    console.log(eigen);
+    console.log('After eig');
+    //console.log(eigen);
     var eigenvalues = eigen.lambda.x;
     var eigenvectors = numeric.transpose(eigen.E.x);
-    console.log(eigenvectors);
+    //console.log(eigenvectors);
 
     var largestEigenvalues = new Array(d);
     for(var i = 0; i < d; i++) {
@@ -58,10 +122,10 @@ function getLargerEigenvectors(matrix, d) {
         });
         largestEigenvalues.pop();
     }
-    console.log(largestEigenvalues);
+    //console.log(largestEigenvalues);
     var selectedEigenvectors = [];
     for(var i = 0; i < d; i++) {
-        console.log(numeric.norm2(eigenvectors[largestEigenvalues[i].idx]));
+        //console.log(numeric.norm2(eigenvectors[largestEigenvalues[i].idx]));
         selectedEigenvectors.push(eigenvectors[largestEigenvalues[i].idx]);
     }
     return selectedEigenvectors;
@@ -85,7 +149,7 @@ function calculateCovarianceMatrix(matrix) {
     for(var i = 0; i < rollCallCount; i++) {
         covarianceMatrix[i] = new Array(rollCallCount).fill(0); 
     }
-
+    //console.log(covarianceMatrix);
     for(var i = 0; i < rollCallCount; i++) {
         for(var j = i; j < rollCallCount; j++) {
             covarianceMatrix[i][j] = calculateCovariance(matrix, i, j, rollCallCount);
@@ -97,7 +161,7 @@ function calculateCovarianceMatrix(matrix) {
             covarianceMatrix[i][j] = covarianceMatrix[j][i];
         }
     }
-    console.log(covarianceMatrix);
+    //console.log(covarianceMatrix);
 
     return covarianceMatrix;
 
